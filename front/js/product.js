ce2e5997ea256ad85a8c4eef61a180ca79257b49
pierre.fayet <piero69450@gmail.com>
2023-01-05 20:19:48 +0100

    wip

--- front/js/product.js
+++ front/js/product.js
@@ -1,12 +1,53 @@
-/*fetch('http://localhost:3000/api/products')
+const url = window.location.search
+const urlParams = new URLSearchParams(url)
+const id = urlParams.get("id")
+
+
+fetch("http://localhost:3000/api/products/" + id)
 .then((response) => response.json())
-const queryString_url_id = window.location.search;
-  const getId = new URLSearchParams(queryString_url_id);
- // let name = params.get('name'); 
-const id = URLSearchParams.get("name");
-console.log(URLSearchParams);
+.then((res) => handleData(res))   
 
-//return params.id; */
+function handleData(kanap) {
+  console.log({kanap})
+const { altTxt, colors, description, imageUrl, name, price, _id} = kanap
+productImg(imageUrl, altTxt)
+productPrice(price)
+productTitle(name)
+productDescription(description)
+productColors(colors)
+}
 
 
+function productImg(imageUrl, altTxt) {
+  const image = document.createElement("img")
+  image.src = imageUrl
+  image.alt = altTxt
+  const addImage = document.querySelector(".item__img")
+  if (addImage != null) addImage.appendChild(image)
+}
+function productPrice(price) {
+  const elPrice = document.querySelector("#price")
+  if (price != null) elPrice.price = elPrice.textContent = price
+}
 
+function productTitle(name) {
+const elTitle  = document.querySelector("#title")
+if (elTitle != null) elTitle.name = elTitle.textContent = name
+}
+
+function productDescription(description) {
+const elDescription = document.querySelector("#description")
+if (elDescription != null) elDescription.description = elDescription.textContent = description
+}
+
+function productColors(colors) {
+  const elColors = document.querySelector("#colors")
+  if(elColors != null) {
+    colors.forEach((color) => {
+      const option = document.createElement("option")
+      // option.value = color
+      // option.textContent = color
+      // Selection.appendChild(option)
+    });
+  }
+}
--- front/js/script.js
+++ front/js/script.js
@@ -1,11 +1,11 @@
-fetch('http://localhost:3000/api/products')
-  .then((response) => response.json())
+fetch("http://localhost:3000/api/products")
+  .then((res) => res.json())
   .then((data) => {
     //console.log(data);
     data.forEach(product => {
       const el = document.createElement('a');
-      el.setAttribute('href',`/product.html?id=${product.id}`);
-      el.innerHTML = `
+      el.setAttribute('href',`./product.html?id=${product._id}`);
+      el.innerHTML += `
       <article>
           <img src="${product.imageUrl}" alt="${product.altTxt}">
           <h3 class="productName">${product.name}"</h3>
